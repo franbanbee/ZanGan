63d9c9e77f277957fc1119c2f6bd805452041bf0
franbanbee <129038669+franbanbee@users.noreply.github.com>
2023-06-12 17:47:51 +0800

    0612
    renew
    手機RWD
    news_content分頁

---
+++ z最新版0612 1745/script.js
@@ -0,0 +1,48 @@
+// cart
+// 課程的數量增減
+var num = 1;//宣告邊數num 變數值為1
+
+$(".minus").click(
+  function () {
+    num = num - 1;
+    $(".num").text(num);
+    check();//執行check裏頭的if與else
+  }
+);
+
+$(".add").click(
+  function () {
+    num = num + 1;
+    $(".num").text(num);
+    check();//執行check裏頭的if與else
+  }
+);
+
+// cart
+// 活動的數量增減
+var num1 = 1;//宣告邊數num 變數值為1
+
+$(".minus1").click(
+  function () {
+    num1 = num1 - 1;
+    $(".num1").text(num1);
+    check();//執行check裏頭的if與else
+  }
+);
+
+$(".add1").click(
+  function () {
+    num1 = num1 + 1;
+    $(".num1").text(num1);
+    check();//執行check裏頭的if與else
+  }
+);
+
+ // topbar綠色的淡出淡入
+ $(window).scroll(function () {
+  if ($(this).scrollTop() > 720) {
+    $('#topbar').stop().fadeTo('', 1);
+  } else {
+    $('#topbar').stop().fadeOut();
+  }
+});
